64861e3d6413b362701e7d889c40cbfead1149ea
Pat Lawler <patlaw777@icloud.com>
2022-04-01 18:09:25 -0500

    updating for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,14 @@
-
+require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('chore_tracker_db', 'root', 'BAngkok2017!!', {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
 module.exports = sequelize;
+
+

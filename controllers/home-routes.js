8b4c58a1ed8b4b6bc1474f281a1cdc6bab276952
Pat Lawler <patlaw777@icloud.com>
2022-04-10 22:26:18 -0500

    added connected handlebars to all pages

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,24 +1,14 @@
 const router = require('express').Router();
 const { Task } = require('../models');
+const withAuth = require('../utils/withAuth');
 
-
-router.get('/dashboard', (req, res) => {
-    res.render('dashboard', req)
-});
-router.get('/login', (req, res) => {
-    res.render('login', req);
-});
-router.get('/admin', (req, res) => {
-    res.render('admin');
-});
-router.get('/', (req, res) => {
-    if(req.session.loggedIn) {
-        res.render('homepage', req);    
-    }
-    else {
-        res.render('login');
-    }
-    
-});
+router.get('/about-us', (req, res) => {res.render('about-us', req)});
+router.get('/dot-services', (req, res) => { res.render('dot-services', req)});
+router.get('/pricing', (req, res) => {res.render('pricing', req)});
+router.get('/schedule', withAuth, (req, res) => { res.render('appointments', req) });
+router.get('/dashboard', withAuth, (req, res) => { res.render('dashboard', req) });
+router.get('/admin', withAuth, (req, res) => { res.render('admin', req) });
+router.get('/login', (req, res) => { res.render('login', req) });
+router.get('/', (req, res) => { res.render('homepage', req) });
 
 module.exports = router;

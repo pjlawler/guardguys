6968688866b2558b85c41dcdbbed3445fdca27ec
Pat Lawler <patlaw777@icloud.com>
2022-04-12 13:47:02 -0500

    added the withAdmin middleware and update menu dropdown divider

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const { Task } = require('../models');
-const withAuth = require('../utils/withAuth');
+const { withAuth, withAdmin} = require('../utils/withAuth');
 
 router.get('/about-us', (req, res) => {res.render('about-us', req)});
 router.get('/dot-services', (req, res) => { res.render('dot-services', req)});
 router.get('/pricing', (req, res) => {res.render('pricing', req)});
 router.get('/schedule', withAuth, (req, res) => { res.render('appointments', req) });
 router.get('/dashboard', withAuth, (req, res) => { res.render('dashboard', req) });
-router.get('/admin', withAuth, (req, res) => { res.render('admin', req) });
+router.get('/admin', withAdmin, (req, res) => { res.render('admin', req) });
 router.get('/login', (req, res) => { res.render('login', req) });
 router.get('/', (req, res) => { res.render('homepage', req) });
 

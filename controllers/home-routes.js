da78a860e4b7e7043860934a787f345ad21b51ee
Pat Lawler <patlaw777@icloud.com>
2022-04-19 22:07:37 -0500

    updated about page and accordian

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,7 @@ const { withAuth, withAdmin} = require('../utils/withAuth');
 
 router.get('/about-us', (req, res) => {res.render('about-us', req)});
 router.get('/dot-services', (req, res) => { res.render('dot-services', req)});
-router.get('/pricing', (req, res) => {res.render('pricing', req)});
+router.get('/prorgram-management', (req, res) => {res.render('program-management', req)});
 router.get('/schedule', withAuth, (req, res) => { res.render('appointments', req) });
 router.get('/dashboard', withAuth, (req, res) => { res.render('dashboard', req) });
 router.get('/admin', withAdmin, (req, res) => { res.render('admin', req) });

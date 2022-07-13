48732c56b666ad082c9a4b53ff5d6a2671310925
Pat Lawler <patlaw777@icloud.com>
2022-07-13 16:33:10 +0700

    updating pages

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,14 @@ const { withAuth, withAdmin} = require('../utils/withAuth');
 const { moreInfo } = require('../utils/helpers');
 
 
-router.get('/more-info', (req, res)=> { res.render('more-info', moreInfo(req))});
+router.get('/background_checks', (req, res)=> {res.render('more-info-background_checks', req)});
+router.get('/faqs', (req, res)=> {res.render('faqs', req)});
+router.get('/contact_us', (req, res)=> {res.render('contact_us', req)});
+router.get('/dna_testing', (req, res)=> {res.render('more-info-dna_testing', req)});
+router.get('/employer_solutions', (req, res)=> {res.render('more-info-employer_solutions', req)});
+router.get('/other_services', (req, res)=> {res.render('more-info-other_services', req)});
+router.get('/drug_alcohol_testing', (req, res)=> {res.render('more-info-drug_alcohol', req)});
+router.get('/dot_programs', (req, res)=> {res.render('more-info-dot_programs', req)});
 router.get('/about-us', (req, res) => {res.render('about-us', req)});
 router.get('/dot-services', (req, res) => { res.render('dot-services', req)});
 router.get('/program-management', (req, res) => {res.render('program-management', req)});

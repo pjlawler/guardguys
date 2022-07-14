6679b6f1c0579e16a5b1e6fade9d7c67a317d8f0
Pat Lawler <patlaw777@icloud.com>
2022-07-14 16:11:59 +0700

    updated much

--- public/javascript/pl_script.js
+++ public/javascript/pl_script.js
@@ -1,5 +1,8 @@
 const panels = document.getElementsByClassName('panel')
 const navbar = document.getElementById('navigation')
+const faq_questions = document.getElementsByClassName('faq-question')
+
+
 function addPanelEventListeners() {
     for(let i=0; i < panels.length; i++) {
         panels[i].addEventListener('click', panelClickHandler)
@@ -9,6 +12,65 @@ function addPanelEventListeners() {
 function addNavBarEventListeners() {
     navbar.addEventListener('click', navbarClickHandler)
 }
+
+function addFaqEventListeners() {
+    for(let i=0; i < faq_questions.length; i++) {
+        faq_questions[i].addEventListener('click', faqClickHandler)
+    }
+}
+
+function faqClickHandler(target) {
+    // gets the element of the clicked item
+    const clickedItem = target.srcElement;
+
+    // checks to see if the element has a specific class in it's classlist
+    const elementHasClass = (element, className) => {
+        const classes = element.className ? element.className.split(' '):[];
+        return (classes.indexOf(className) !== -1 )
+    }
+
+    // gets the element for the faq
+    const elFaqQuestion = () => {
+        let parent = clickedItem;
+
+        // finds the top element of the faq box
+        do {
+            if(elementHasClass(parent, 'faq-box')) break;
+            parent = parent.parentNode ? parent.parentNode : null
+        } while(parent)
+
+        // if the panel is not found or the panel does not have child emements returns false
+        if(!parent.hasChildNodes()) return false;
+
+        // goes through the child elements to get the faq-answer
+        for(let i=0; i < parent.childNodes.length -1; i++) {
+            if(elementHasClass(parent.childNodes[i], 'faq-answer')) return parent.childNodes[i];
+        }
+    }
+
+    // handles action if the faq-toggle was clicked
+    if(elementHasClass(clickedItem, 'faq-toggle')) {
+
+        // ensure the faq-question is present
+        const faqQuestion = elFaqQuestion();
+        if (!faqQuestion) return false
+
+        switch(elementHasClass(faqQuestion, 'isHidden')) {
+            case true: // shows answer
+                faqQuestion.classList.remove('isHidden')
+                clickedItem.src = '/assets/images/minus.png'
+                break;
+            case false: // hides answer answer
+                faqQuestion.classList.add('isHidden')
+                clickedItem.src = '/assets/images/plus.png'
+                break;
+        }
+}
+    }
+
+    
+
+
 
 function panelClickHandler(target) {
     
@@ -76,3 +138,4 @@ function navbarClickHandler(target) {
 
 addPanelEventListeners();
 addNavBarEventListeners();
+addFaqEventListeners()
